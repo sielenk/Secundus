b735ad0f30873fdb08cb21a487718c8248cf43c8
Marvin H. Sielenkemper <sielenk@gmx.de>
2017-12-16 19:09:38 +0100

    initial commit

---
+++ app/src/main/java/de/masitec/secundus/MainActivity.kt
@@ -0,0 +1,12 @@
+package de.masitec.secundus
+
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
