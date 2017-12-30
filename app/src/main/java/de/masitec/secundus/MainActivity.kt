ad482a03409902fd35f162456c49a9ff9f246049
Marvin H. Sielenkemper <sielenk@gmx.de>
2017-12-30 19:19:50 +0100

    moved the fragments into the page adapter, used framework code to initialize the tabs

--- app/src/main/java/de/masitec/secundus/MainActivity.kt
+++ app/src/main/java/de/masitec/secundus/MainActivity.kt
@@ -5,39 +5,20 @@ import android.support.design.widget.TabLayout
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import de.masitec.secundus.fragments.CameraFragment
-import de.masitec.secundus.fragments.SmellButtonFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val fragments = listOf(
-                Pair(R.string.smell_button_tab, { SmellButtonFragment() }),
-                Pair(R.string.camera_tab, { CameraFragment() })
-        )
-
         val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
-        fragments.forEach {
-            tabLayout.addTab(tabLayout.newTab().setText(it.first))
-        }
         tabLayout.tabGravity = TabLayout.GRAVITY_FILL
 
         val viewPager = findViewById<ViewPager>(R.id.pager)
-        val adapter = SecundusPageAdapter(supportFragmentManager, fragments.map { it.second })
-        viewPager.adapter = adapter
-        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
-        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabReselected(tab: TabLayout.Tab) {}
-
-            override fun onTabUnselected(tab: TabLayout.Tab) {}
-
-            override fun onTabSelected(tab: TabLayout.Tab) {
-                viewPager.setCurrentItem(tab.position);
-            }
-        })
+        viewPager.adapter = SecundusPageAdapter(supportFragmentManager, resources)
+        tabLayout.setupWithViewPager(viewPager)
     }
 }
--- app/src/main/java/de/masitec/secundus/SecundusPageAdapter.kt
+++ app/src/main/java/de/masitec/secundus/SecundusPageAdapter.kt
@@ -1,15 +1,28 @@
 package de.masitec.secundus
 
+import android.content.res.Resources
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.app.FragmentStatePagerAdapter
+import de.masitec.secundus.fragments.CameraFragment
+import de.masitec.secundus.fragments.SmellButtonFragment
 
-/**
- * Created by Marv on 30.12.17.
- */
-class SecundusPageAdapter(fm: FragmentManager, val fragments: List<() -> Fragment>) : FragmentPagerAdapter(fm) {
+class SecundusPageAdapter(fm: FragmentManager, val resources: Resources)
+    : FragmentStatePagerAdapter(fm) {
+
+    private companion object {
+        val fragments = listOf(
+                Pair(R.string.smell_button_tab, { SmellButtonFragment() }),
+                Pair(R.string.camera_tab, { CameraFragment() })
+        )
+    }
+
     override fun getItem(position: Int): Fragment {
-        return fragments[position]()
+        return fragments[position].second()
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return resources.getText(fragments[position].first)
     }
 
     override fun getCount(): Int {
